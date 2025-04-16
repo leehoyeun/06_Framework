0cc8d31676423669717b8832ec68ddf7a8389655
leehoyeun <dlghdus100@gmail.com>
2025-04-16 17:51:01 +0900

    투두까지

--- demoProject2/src/main/java/edu/kh/demo/controller/ExampleController.java
+++ demoProject2/src/main/java/edu/kh/demo/controller/ExampleController.java
@@ -6,10 +6,15 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import edu.kh.demo.model.dto.Student;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping("example") //    /example 로 시작하는 주소를 해당 컨트롤러에 매핑
@@ -47,8 +52,71 @@ public class ExampleController {
 		
 		model.addAttribute("fruitList",fruitList);
 		
+		//DTO 객체 Model을 이용해서 html로 전달
+		Student std = new Student();
+		std.setStudentNo("12345");
+		std.setName("홍길동");
+		std.setAge(22);
 		
+		model.addAttribute("std",std);
+		
+		//List<Student> 객체 Model을 이용해서 html로 전달
+		List<Student> stdList = new ArrayList<>();
+		
+		stdList.add(new Student("11111","김일번",20));
+		stdList.add(new Student("22222","이이번",23));
+		stdList.add(new Student("33333","홍삼섬",22));
+		
+		model.addAttribute("stdList",stdList);
 		//src/main/resources/templates/example/ex1.html로 forward
 		return "example/ex1";
+	}
+		
+	@PostMapping("ex2")	
+	public String ex2(Model model) {
+	
+		model.addAttribute("str","<h1>테스트 중 &times; </h1>");
+		
+		return "example/ex2";
+	}
+	
+	@GetMapping("ex3")
+	public String ex3(Model model) {
+		model.addAttribute("key","제목");
+		model.addAttribute("query","검색어");
+		model.addAttribute("boardNo",10);
+		return "example/ex3";
+	}
+	@GetMapping("ex3/{path}")
+	public String pathVariableTest(@PathVariable("path") int path) {
+		// controller에서 해야하는일 동일한 경우
+		// example/ex3/1  , exapmple/ex3/2,    example/ex3/3 ...
+		// 주소 중{path} 부분의 값을 가져와서 매개변수로 저장
+		// controller단의 메서드에서 사용할 수 있도록 해줌
+		// request scope 자동 세팅
+		
+		log.debug("path :"+path);
+		
+		return "/example/testResult";
+	}
+	@GetMapping("ex4")
+	public String ex4(Model model) {
+		
+		Student std = new Student("67890","잠만보",22);
+		model.addAttribute("std",std);
+		model.addAttribute("num",100);
+		return "/example/ex4";
+	}
+	
+	@GetMapping("ex5")
+	public String ex5(Model model) {
+		model.addAttribute("message","타임리프 + Javascript 사용연습");
+		model.addAttribute("num1",12345);
+		
+		Student std = new Student();
+		std.setStudentNo("22222");
+		model.addAttribute("std",std);
+		
+		return "example/ex5";
 	}
 }
